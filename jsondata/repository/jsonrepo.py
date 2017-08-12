eaf418f69ed87f182f61b463eff0b5fb89c1b138
haraprasadj <haraprasad@gmail.com>
2017-08-12 09:43:06 +0530

    heroku

--- jsondata/repository/jsonrepo.py
+++ jsondata/repository/jsonrepo.py
@@ -1,5 +1,6 @@
 from jsondata.service.testdataservice import get_test_data
 import os
+import json
 
 
 class JsonRepository:
@@ -9,11 +10,17 @@ class JsonRepository:
 
     def get_json_data(self, filename):
         filepath = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'generated_files',
-                                filename)
+                                filename + '.json')
         with open(filepath, 'a') as f:
-            for _ in range(self._length):
-                res = {}
-                for field in self._fields:
-                    res[field['name']] = get_test_data(field['type'], 1)[0]
-                f.write(res.__repr__() + ',\n')
+            f.write('[')
+            for _ in range(self._length - 1):
+                f.write(self._get_json_item() + ',')
+            f.write(self._get_json_item())
+            f.write(']')
         return filepath
+
+    def _get_json_item(self):
+        res = {}
+        for field in self._fields:
+            res[field['name']] = get_test_data(field['type'], 1)[0]
+        return json.dumps(res)
