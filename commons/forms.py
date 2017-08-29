e2a247e2664679b3e7e0af5e43e2110f4766c2ab
haraprasadj <haraprasad.jvr@gmail.com>
2017-08-29 08:51:24 +0530

    adding bootstrap

--- commons/forms.py
+++ commons/forms.py
@@ -4,11 +4,13 @@ from commons.config.datatype import DataType
 
 
 class DataSpecForm(forms.Form):
-    data_length = forms.IntegerField(label="Number of records to generate", max_value=50)
-    field_length = forms.IntegerField(label="Number of fields per record", max_value=5)
+    data_length = forms.IntegerField(label="Number of records to generate", max_value=50,
+                                     widget=forms.TextInput(attrs={'class': 'form-control'}))
+    field_length = forms.IntegerField(label="Number of fields per record", max_value=5,
+                                      widget=forms.TextInput(attrs={'class': 'form-control'}))
 
 
 class FieldSpecForm(forms.Form):
     data_type = forms.ChoiceField([(name, name) for name, member in DataType.__members__.items()],
                                   widget=forms.Select(attrs={'class': 'regDropDown'}))
-    field_name = forms.CharField()
+    field_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
